3553acd1ca437172e49a95218c339d5d2e6e46ad
Benanna2019 <bass41992ben@gmail.com>
2020-11-11 08:37:03 -0500

    updated routes

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
-//GET ROUTES
-//- Customer Info
-//- Created Invoice
+require("dotenv").config();
+const express = require("express");
+const sql = require("mysql2/promise");
+const cors = require("cors");
+const PORT = 4000;
+const authorizeUser = require("./authorization/middleware");
+const aws = require("aws-sdk");
+
+// aws.confiq.setPromisesDependency();
+// aws.config.update({
+//   accessKeyId: process.env.Access_Key_ID,
+//   secretAccessKey: process.env.Secret_Access_Key,
+//   region: "us-east-1",
+// });
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+const pool = sql.createPool({
+  host: process.env.host,
+  user: process.env.sqluser,
+  password: process.env.password,
+});
+
 //- User info - This would be address and stuff for bottom of invoice
 app.post("/user", authorizeUser, async (req, resp) => {
   console.log("get user hit");
@@ -19,6 +42,7 @@ app.post("/user", authorizeUser, async (req, resp) => {
     console.log(error);
   }
 });
+
 //- Invoice Data for STATS and GRAPHS
 // -- such as Total amounts of invoices
 // -- total amount of fulfilled invoices
@@ -34,7 +58,7 @@ app.post("/create-user", authorizeUser, async (req, resp) => {
     const username = req.decodedToken["cognito:username"];
     //figure out if I want name/company name and address
     const response = await conn.execute(
-      "INSERT INTO invoiceDb.invoices (username) VALUES (?)",
+      "INSERT INTO invoiceDb.users (username) VALUES (?)",
       [username]
     );
 
@@ -47,59 +71,72 @@ app.post("/create-user", authorizeUser, async (req, resp) => {
 });
 
 // - Update user - for updates in user info
-app.post("/update-user", authorizeUser, async (req, resp) => {
-  console.log("update user hit");
-  try {
-    const username = req.decodedToken["cognito:username"];
-    const conn = await pool.getConnection();
-    //getting original user data
-    const foo = await conn.execute(
-      "SELECT * FROM invoiceDb.users WHERE username=?",
-      [username]
-    );
-    const oldData = foo[0][0];
-    //if entered data is empty store old data and if not store new data
-    const name =
-      req.body.name === "" || undefined ? oldData.name : req.body.name;
-    const companyName =
-      req.body.companyName === "" || undefined
-        ? oldData.companyName
-        : req.body.companyName;
-    const companyAddress =
-      req.body.companyAddress === "" || undefined
-        ? oldData.companyAddress
-        : req.body.companyAddress;
-    console.log(name);
-    console.log(companyName);
-    console.log(companyAddress);
-    console.log(oldData);
-    const response = await conn.execute(
-      "UPDATE invoiceDb.users SET name=?, companyName=?, companyAddress=? WHERE username=?",
-      [name, companyName, companyAddress, username]
-    );
-    conn.release();
-    resp.status(201).send(response);
-  } catch (error) {
-    console.log(error);
-    resp.status(500).send(error);
-  }
-});
+// app.post("/update-user", authorizeUser, async (req, resp) => {
+//   console.log("update user hit");
+//   try {
+//     const username = req.decodedToken["cognito:username"];
+//     const conn = await pool.getConnection();
+//     //getting original user data
+//     const foo = await conn.execute(
+//       "SELECT * FROM invoiceDb.users WHERE username=?",
+//       [username]
+//     );
+//     const oldData = foo[0][0];
+//     //if entered data is empty store old data and if not store new data
+//     const name =
+//       req.body.name === "" || undefined ? oldData.name : req.body.name;
+//     const companyName =
+//       req.body.companyName === "" || undefined
+//         ? oldData.companyName
+//         : req.body.companyName;
+//     const companyAddress =
+//       req.body.companyAddress === "" || undefined
+//         ? oldData.companyAddress
+//         : req.body.companyAddress;
+//     console.log(name);
+//     console.log(companyName);
+//     console.log(companyAddress);
+//     console.log(oldData);
+//     const response = await conn.execute(
+//       "UPDATE invoiceDb.users SET name=?, companyName=?, companyAddress=? WHERE username=?",
+//       [name, companyName, companyAddress, username]
+//     );
+//     conn.release();
+//     resp.status(201).send(response);
+//   } catch (error) {
+//     console.log(error);
+//     resp.status(500).send(error);
+//   }
+// });
 
 // - Create a new customer
 app.post("/create-customer", authorizeUser, async (req, resp) => {
   console.log("create customer hit");
   try {
-    const username = req.decodedToken["cognito:username"];
+    const customer_name = req.body.customerName;
+    const company = req.body.companyName;
+    const client_of = req.decodedToken["cognito:username"];
+    const address_street = req.body.addressStreet;
+    const address_city = req.body.city;
+    const address_state = req.body.state;
+    const address_zip = req.body.zipcode;
     //we want the unique username because the user has their own customers.
     //we do not want other people to be able to login and simply have access to every customer in the DB
     //I think
     const conn = await pool.getConnection();
-    const customerName = req.body.customerName;
 
     const response = await conn.execute(
       //Do I need to include the username in the customer table?
-      "INSERT INTO invoiceDb.customers (customerName) VALUES (?)",
-      [customerName]
+      "INSERT INTO invoiceDb.customers (customer_name, company, client_of, address_street, address_city, address_state, address_zip) VALUES (?,?,?,?,?,?,?)",
+      [
+        customer_name,
+        company,
+        client_of,
+        address_street,
+        address_city,
+        address_state,
+        address_zip,
+      ]
     );
 
     conn.release();
@@ -111,165 +148,180 @@ app.post("/create-customer", authorizeUser, async (req, resp) => {
 });
 
 // - Update Customer Info
-app.post("/update-customer", authorizeUser, async (req, resp) => {
-  console.log("update customer hit");
-  try {
-    const username = req.decodedToken["cognito:username"];
-    const conn = await pool.getConnection();
-    const customerName = req.body.customerName;
+// app.post("/update-customer", authorizeUser, async (req, resp) => {
+//   console.log("update customer hit");
+//   try {
+//     const username = req.decodedToken["cognito:username"];
+//     const conn = await pool.getConnection();
+//     // const customerName = req.body.customerName;
 
-    const foo = await conn.execute(
-      "SELECT * FROM invoiceDb.customers WHERE username=?",
-      [username]
-    );
-    const oldData = foo[0][0];
-    //if entered data is empty store old data and if not store new data
-    const customerName =
-      req.body.customerName === "" || undefined
-        ? oldData.customerName
-        : req.body.customerName;
-    const customerAddress =
-      req.body.customerAddress === "" || undefined
-        ? oldData.customerAddress
-        : req.body.customerAddress;
-    const email =
-      req.body.email === "" || undefined ? oldData.email : req.body.email;
-    console.log(customerName);
-    console.log(customerAddress);
-    console.log(email);
-    console.log(oldData);
+//     const foo = await conn.execute(
+//       "SELECT * FROM invoiceDb.customers WHERE username=?",
+//       [username]
+//     );
+//     const oldData = foo[0][0];
+//     //if entered data is empty store old data and if not store new data
+//     const customerName =
+//       req.body.customerName === "" || undefined
+//         ? oldData.customerName
+//         : req.body.customerName;
+//     const customerAddress =
+//       req.body.customerAddress === "" || undefined
+//         ? oldData.customerAddress
+//         : req.body.customerAddress;
+//     const email =
+//       req.body.email === "" || undefined ? oldData.email : req.body.email;
+//     console.log(customerName);
+//     console.log(customerAddress);
+//     console.log(email);
+//     console.log(oldData);
+//     const response = await conn.execute(
+//       "UPDATE invoiceDb.customers SET customerName=?, customerAddress=?, email=? WHERE username=?",
+//       [customerName, customerAddress, email, username]
+//     );
+//     conn.release();
+//     resp.status(201).send(response);
+//   } catch (error) {
+//     console.log(error);
+//     resp.status(500).send({ message: error });
+//   }
+// });
+
+// - Create a new invoice
+// - I need to reference customer_name, company
+// - and address info when making a new invoice
+// - I think I would just join 'billTo' from invoice
+// - and 'address' from customers in the query
+app.post("/create-invoice", authorizeUser, async (req, resp) => {
+  console.log("create invoice hit");
+  try {
+    const invoice_uuid = req.body.invoiceUuid;
+    const todays_date = Date.now();
+    const bill_to = req.body.billTo;
+    const user_id = req.decodedToken["cognito:username"];
+
+    const conn = await pool.getConnection();
     const response = await conn.execute(
-      "UPDATE invoiceDb.customers SET customerName=?, customerAddress=?, email=? WHERE username=?",
-      [customerName, customerAddress, email, username]
+      "INSERT INTO invoiceDb.invoices (invoice_uuid, todays_date, bill_to, user_id) VALUES (?,?,?,?)",
+      [invoice_uuid, todays_date, bill_to, user_id]
     );
+
     conn.release();
     resp.status(201).send(response);
   } catch (error) {
-    console.log(error);
+    console.log("This is your error", error);
     resp.status(500).send({ message: error });
   }
 });
 
-// - Create a new invoice
-app.post("/create-invoice", authorizeUser, async (req, resp) => {
-  console.log("create invoice hit");
+//adding an order route
+app.post("/add-order", authorizeUser, async (req, resp) => {
+  console.log("add order hit");
   try {
-    const invoiceUuid = req.body.invoiceUuid;
-    const date = Date.now();
-    const billTo = req.body.customerName;
-    const itemDescription = req.body.itemDescription;
     const quantity = req.body.quantity;
-    const pricePerItem = req.body.pricePerItem;
-    const itemSumAmt = req.body.itemSumAmt; //total for that item
-    const totalAmount = req.body.totalAmount; //total for all items
-
+    const cost = req.body.cost;
+    const order_description = req.body.itemDescription;
+    const invoice_uuid = req.body.invoiceUuid;
+    const user_id = req.decodedToken["cognito:username"];
     const conn = await pool.getConnection();
     const response = await conn.execute(
-      "INSERT INTO invoiceDb.invoices (invoiceUuid, date, billTo, itemDescription, quantity, pricePerItem, itemSumAmt, TotalAmount) VALUES (?,?,?,?,?,?,?,?)",
-      [
-        invoiceUuid,
-        date,
-        billTo,
-        itemDescription,
-        quantity,
-        pricePerItem,
-        itemSumAmt,
-        totalAmount,
-      ]
+      "INSERT INTO invoiceDb.orders (quantity, cost, order_description, invoice_uuid, user_id) VALUES (?,?,?,?,?)",
+      [quantity, cost, order_description, invoice_uuid, user_id]
     );
 
     conn.release();
     resp.status(201).send(response);
   } catch (error) {
-    console.log(error);
-    resp.status(500).send(error);
+    console.log("This is your error", error);
+    resp.status(500).send({ message: error });
   }
 });
 
 // - Update Invoice
-app.post("/update-invoice", authorizeUser, async (req, resp) => {
-  console.log("update invoice hit");
-  try {
-    const invoiceUuid = req.body.invoiceUuid;
-    const date = Date.now();
-    const billTo = req.body.customerName;
-    const itemDescription = req.body.itemDescription;
-    const quantity = req.body.quantity;
-    const pricePerItem = req.body.pricePerItem;
-    const itemSumAmt = req.body.itemSumAmt; //total for that item
-    const totalAmount = req.body.totalAmount; //total for all items
+// app.post("/update-invoice", authorizeUser, async (req, resp) => {
+//   console.log("update invoice hit");
+//   try {
+//     const invoiceUuid = req.body.invoiceUuid;
+//     const date = Date.now();
+//     const billTo = req.body.customerName;
+//     const itemDescription = req.body.itemDescription;
+//     const quantity = req.body.quantity;
+//     const pricePerItem = req.body.pricePerItem;
+//     const itemSumAmt = req.body.itemSumAmt; //total for that item
+//     const totalAmount = req.body.totalAmount; //total for all items
 
-    const conn = await pool.getConnection();
-    const response = await conn.execute(
-      "UPDATE invoiceDb.invoices SET invoiceUuid=?, date=?, billTo=?, itemDescription=?, quantity=?, pricePerItem=?, itemSumAmt=?, TotalAmount=?) VALUES (?,?,?,?,?,?,?,?)",
-      [
-        invoiceUuid,
-        date,
-        billTo,
-        itemDescription,
-        quantity,
-        pricePerItem,
-        itemSumAmt,
-        totalAmount,
-      ]
-    );
+//     const conn = await pool.getConnection();
+//     const response = await conn.execute(
+//       "UPDATE invoiceDb.invoices SET invoiceUuid=?, date=?, billTo=?, itemDescription=?, quantity=?, pricePerItem=?, itemSumAmt=?, TotalAmount=?) VALUES (?,?,?,?,?,?,?,?)",
+//       [
+//         invoiceUuid,
+//         date,
+//         billTo,
+//         itemDescription,
+//         quantity,
+//         pricePerItem,
+//         itemSumAmt,
+//         totalAmount,
+//       ]
+//     );
 
-    conn.release();
-    resp.status(201).send(response);
-  } catch (error) {
-    console.log(error);
-    resp.status(500).send(error);
-  }
-});
+//     conn.release();
+//     resp.status(201).send(response);
+//   } catch (error) {
+//     console.log(error);
+//     resp.status(500).send(error);
+//   }
+// });
 
 // - Delete Invoice
-app.post("/delete-invoice", authorizeUser, async (req, resp) => {
-  console.log("delete invoice hit");
-  try {
-    const invoiceId = req.body.invoiceId;
-    const conn = await pool.getConnection();
+// app.post("/delete-invoice", authorizeUser, async (req, resp) => {
+//   console.log("delete invoice hit");
+//   try {
+//     const invoiceId = req.body.invoiceId;
+//     const conn = await pool.getConnection();
 
-    const response = await conn.execute(
-      "DELETE FROM invoiceDb.invoices WHERE invoiceId=?",
-      [invoiceId]
-    );
+//     const response = await conn.execute(
+//       "DELETE FROM invoiceDb.invoices WHERE invoiceId=?",
+//       [invoiceId]
+//     );
 
-    conn.release();
-    resp.status(200).send({ message: "successfully deleted" });
-  } catch (error) {
-    resp.status(500).send(error);
-    console.log(error);
-  }
-});
+//     conn.release();
+//     resp.status(200).send({ message: "successfully deleted" });
+//   } catch (error) {
+//     resp.status(500).send(error);
+//     console.log(error);
+//   }
+// });
 
 // - get all user invoices
-app.post("/get-user-invoices", authorizeUser, async (req, resp) => {
-  console.log("get user invoices hit");
-  try {
-    const username = req.decodedToken["cognito:username"];
-    const conn = await pool.getConnection();
-    const response = await conn.execute(
-      "SELECT * FROM invoiceDb.invoices WHERE username=?",
-      [username]
-    );
+// - need another FOREIGN KEY of customer_id?
+// app.post("/get-user-invoices", authorizeUser, async (req, resp) => {
+//   console.log("get user invoices hit");
+//   try {
+//     const username = req.decodedToken["cognito:username"];
+//     const conn = await pool.getConnection();
+//     const response = await conn.execute(
+//       "SELECT * FROM invoiceDb.invoices WHERE username=?",
+//       [username]
+//     );
 
-    conn.release();
-    resp.status(200).send(response[0]);
-  } catch (error) {
-    console.log(error);
-    resp.status(500).send(error);
-  }
-});
+//     conn.release();
+//     resp.status(200).send(response[0]);
+//   } catch (error) {
+//     console.log(error);
+//     resp.status(500).send(error);
+//   }
+// });
 
 // - search customers - to be able to fill in invoice with customer info
-app.post("/search", authorizeUser, async (req, resp) => {
+app.post("/customer-search", authorizeUser, async (req, resp) => {
   console.log("get search customer hit");
   try {
-    const search = req.body.search;
+    const client_of = req.decodedToken["cognito:username"];
     const conn = await pool.getConnection();
     const response = await conn.execute(
-      "SELECT * FROM invoiceDb.customers WHERE name LIKE ?",
-      ["%" + search + "%"]
+      "SELECT customer_name FROM invoiceDb.customers WHERE client_of = ?",
+      [client_of]
     );
     conn.release();
     resp.status(200).send(response[0]);
@@ -279,3 +331,5 @@ app.post("/search", authorizeUser, async (req, resp) => {
   }
 });
 // - S3 route for PDF's and .txt files
+
+app.listen(PORT, () => console.log("app is listing on", PORT));
--- authorization/middleware.js
+++ authorization/middleware.js
@@ -1,2 +1,55 @@
 //authorization middleware
 //jwk for aws cognito authorization
+const jwt = require("jsonwebtoken");
+const jwkToPem = require("jwk-to-pem");
+
+function authorizeUser(req, resp, next) {
+  console.log("authorization hit");
+  if (req.body.token == null) {
+    console.log("token is undefined");
+    return resp.status(401).send();
+  }
+  //jwk keys go here
+  //jwk -> tokenid -> pem -> verification between jwt and pem
+
+  const jwk = {
+    keys: [
+      {
+        alg: "RS256",
+        e: "AQAB",
+        kid: "1Bf/th1d1QSEO3ZyVviXgx6ZqtvKwbLxa/TdAhTS5mU=",
+        kty: "RSA",
+        n:
+          "jT8b2PtsCFrAcpZkbi5cD-Qo7mj6XZ_zofMzLAMfkqwGTV8BMQC2awDn1uPjyXAZYYKYn6xgqe27HOWRiUzU_DP-4B3mfJkEc_-bi9Ci8-LP4SMShPV4ASr7CSm8OvfFJN6WjIajaL6fYAHJdJDXleg0-zfg4LcX078WXXW3ZkWd4Pex9-M5ja1ahfQvJ_tgveAs-jN8LRrwdNvkHrHRHUGFiNeRKXuLUx3jAyerlnhI48I1YWGdnSrZ3WmiiHIvJBbTETq3CbRDDinKS4uuikL8PpiangyMb_-yedwA5BHMLo8UhpwA1VLOx692sfIwSL3dACogA0XY2dseO_LGmQ",
+        use: "sig",
+      },
+      {
+        alg: "RS256",
+        e: "QAB",
+        kid: "YJOXoxsMiZvXx/k5ah6IM1VftMIffHIpSXcgMzuExMM=",
+        kty: "RSA",
+        n:
+          "mg2880GUt0Eb0QefUsiHEVm8C5Vm6sKBPSniAqR5MCM9KD-QbAZEuSW0B_iVQ9-QGkblE3EIfM24cCEKOFQ86_phLpiaHckYaKDsZFFcfJox4H9pQY7wlPay0ANkHMlLyEVYHj0zWZ0geVdSu8-W-TEkn6zVDgRtfn19KU8PVsxHPC1wcec2yuT6-fr5tY64SV76Q-M1hq7LHnbidcukjcbj4sXmwHc0R6IsJZvrE9VuEUrPFbdJ257f8SxASxvpplNwEN43605_mB_aI0sQN7tH2kutZNexl-S8T2SH9Vu6hUyCjuupkLe8hyLwAPTslse_ndF2jNAlSO12IC2zEw",
+        use: "sig",
+      },
+    ],
+  };
+
+  const jwkForIdToken = jwk.keys[0];
+  const pem = jwkToPem(jwkForIdToken);
+  try {
+    jwt.verify(req.body.token, pem, (error, decodedToken) => {
+      if (error) {
+        console.log(error);
+        return res.status(403).send(error);
+      }
+      // console.log('decoded token', decodedToken);
+      req.decodedToken = decodedToken;
+      next();
+    });
+  } catch (error) {
+    return resp.status(500).send(error);
+  }
+}
+
+module.exports = authorizeUser;
